8fc2d4bfdaebe46641ad2c9ff9d51b7434d0e053
Dominic Brown <domingomonk@gmail.com>
2016-01-07 23:01:11 -0500

    This is a game to loop around an shoot stuff. Init commit. I am very tired

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float maxSpeed = 10f;
+	public float maxTurning = 2;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	
+	private float nextFire;
+
+	[System.Serializable]
+	public class Boundary
+	{
+		public float xMin, xMax, yMin, yMax;
+	}
+
+	void Start()
+	{
+		rb.GetComponent<Rigidbody> ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float speed = Input.GetAxis ("Vertical") * maxSpeed * Time.deltaTime;
+		float turning = Input.GetAxis ("Horizontal") * maxTurning * Time.deltaTime;
+
+		if (Input.GetKey("left")== false && Input.GetKey("right") == false) { //stops turning momentum when they let go of the button
+			rb.angularVelocity = Vector3.zero;
+		} 
+
+		rb.AddForce (transform.up * speed, ForceMode.Acceleration);
+		rb.AddTorque (transform.forward * turning * -1); //-1 makes it turn the right way
+
+		rb.position = new Vector3 //Sets boundaries
+			(
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+				Mathf.Clamp (GetComponent<Rigidbody>().position.y, boundary.yMin, boundary.yMax),
+				0.0f 
+
+				);
+
+	}
+}
---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour {
+
+	public float speed;
+
+	public Collider collider;
+	public Rigidbody rb;
+	
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		collider = GetComponent<Collider> ();
+
+		rb.velocity = transform.up * speed;
+	}
+	
+	void OnCollisionEnter(Collider other)
+	{
+
+	}
+}
