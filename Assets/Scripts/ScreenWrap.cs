e1216f6a66da647a422500598dfb620f7cdc0bf0
Dominic Brown <domingomonk@gmail.com>
2016-01-11 23:54:11 -0500

    Started ghost object creation module. Currently crashes unity. Consider spliting into two differnet modules.

--- Assets/Scripts/ScreenWrap.cs
+++ Assets/Scripts/ScreenWrap.cs
@@ -4,11 +4,24 @@ using System.Collections;
 public class ScreenWrap : MonoBehaviour {
 
 	public Renderer render;
-	public GameObject ghost;
+	public Rigidbody original;
+	public Transform ghost;
+	public GhostPositions ghostsPos;
+
+	[System.Serializable]
+	public class GhostPositions
+	{
+		public float xMin, xMax, yMin, yMax;
+	}
 
 	void Start()
 	{
 		render = GetComponent<Renderer> ();
+		ghost = GetComponent<Transform> ();
+		original = GetComponent<Rigidbody> (); 
+
+		CreateGhostObjects ();
+		
 	}
 	private bool CheckRenderers(Renderer render)
 	{
@@ -24,5 +37,19 @@ public class ScreenWrap : MonoBehaviour {
 			return false;
 		}
 	}
-	//private void CreateGhostObjects(prefab)
+	private void CreateGhostObjects() //VERY BROKEN
+	{
+		Vector3 originalPosition = new Vector3 (original.transform.position.x, original.transform.position.y, 0.0f);
+
+		Instantiate (ghost,new Vector3(originalPosition.x + ghostsPos.xMin, originalPosition.y + ghostsPos.yMax,0.0f),original.rotation); //upper left ghost
+		Instantiate (ghost,new Vector3(originalPosition.x, originalPosition.y + ghostsPos.xMax,0.0f),original.rotation); // upper middle ghost
+		Instantiate (ghost,new Vector3(originalPosition.x + ghostsPos.xMax, originalPosition.y + ghostsPos.yMax,0.0f),original.rotation); //upper right ghost
+
+		Instantiate (ghost,new Vector3(originalPosition.x + ghostsPos.xMin, originalPosition.y,0.0f),original.rotation); //middle left ghost
+		Instantiate (ghost,new Vector3(originalPosition.x + ghostsPos.xMax, originalPosition.y,0.0f),original.rotation); //middle right ghost
+
+		Instantiate (ghost,new Vector3(originalPosition.x + ghostsPos.xMin, originalPosition.y + ghostsPos.yMin,0.0f),original.rotation); //lower left ghost
+		Instantiate (ghost,new Vector3(originalPosition.x, originalPosition.y + ghostsPos.yMin,0.0f),original.rotation); //lower middle ghost
+		Instantiate (ghost,new Vector3(originalPosition.x + ghostsPos.xMax, originalPosition.y + ghostsPos.yMin,0.0f),original.rotation); //lower right ghost
+	}
 }
